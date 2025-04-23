692d5157f0bb61d2876b54b53cc1fbd8b600f60f
Lauany Bernardo <lauany.sibaldeli@edu.unipar.br>
2025-04-23 20:24:43 -0300

    Add files via upload

--- exemplo java/exemplo 2bimestre/script.js
+++ exemplo java/exemplo 2bimestre/script.js
@@ -1,3 +1,18 @@
 let titulo = document.querySelector("#titulo");
 let campoTexto = document.querySelector("#campoTexto");
-let trocarTexto = document.querySelector("#trocarTexto");
+let trocarTexto = document.querySelector("#bttrocarTexto");
+
+function alterartexto (){
+    //retirando o valor digitado no input
+    //e jogando na variavel
+    let textodigitado = campoTexto.value;
+
+    //atribuindo ao elemento titulo o texto que foi digitado
+    //no input
+    titulo.textContent = textodigitado;
+}
+
+//atribuindo uma ação ao clicar no botao
+bttrocarTexto.onclick = function(){
+    alterartexto();
+}
